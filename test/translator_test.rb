223aad4e196adba4fd3b5d3c211d04270c97d170
Tara Craig <tara.craig7@gmail.com>
2018-05-09 09:28:53 -0600

    Add initialize and english to morse tests

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -1,1 +1,15 @@
-# YOUR TESTS GO IN HERE!
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/translator"
+
+class TranslatorTest < Minitest::Test
+  def test_it_exists
+    translator = Translator.new
+    assert_instance_of Translator, translator
+  end
+
+  def test_eng_to_morse
+    translator = Translator.new("hello world")
+    assert_equal "......-...-..--- .-----.-..-..-.." translator.eng_to_morse
+  end
+end
